9d2d6d2ed38c4108f31bc27d7b23b4afb68a732f
Mikael Karon <mikael@karon.se>
2019-06-01 20:48:27 +0200

    feat: allow children for functional elements

--- src/components/xstate/index.tsx
+++ src/components/xstate/index.tsx
@@ -6,6 +6,7 @@ import {
   InterpreterOptions
 } from 'xstate';
 
+
 export { State as MachineState } from 'xstate'
 
 export interface MachineOptions extends Partial<InterpreterOptions> {
@@ -18,13 +19,13 @@ export type MachineProps<TContext, TSchema, TEvent extends EventObject> = {
   renderer?: MachineRenderer<TContext, TSchema, TEvent>;
 };
 
-export const Machine = <TContext, TSchema, TEvent extends EventObject>({ machine, options }: MachineProps<TContext, TSchema, TEvent>) => <xstate-machine machine={machine} options={options} />
+export const Machine = <TContext, TSchema, TEvent extends EventObject>({ machine, options, renderer }: MachineProps<TContext, TSchema, TEvent>, children?: Element[]) => <xstate-machine machine={machine} options={options} renderer={renderer}>{...children}</xstate-machine>
 
 export type ServiceProps<TContext, TSchema, TEvent extends EventObject> = {
   service: Interpreter<TContext, TSchema, TEvent>;
   renderer?: MachineRenderer<TContext, TSchema, TEvent>;
 };
 
-export const Service = <TContext, TSchema, TEvent extends EventObject>({ service }: ServiceProps<TContext, TSchema, TEvent>) => <xstate-service service={service} />
+export const Service = <TContext, TSchema, TEvent extends EventObject>({ service }: ServiceProps<TContext, TSchema, TEvent>, children?: Element[]) => <xstate-service service={service}>{...children}</xstate-service>
 
 export type MachineRenderer<TContext, TSchema, TEvent extends EventObject> = (current: State<TContext>, send: Interpreter<TContext, TSchema, TEvent>['send'], service: Interpreter<TContext, TSchema, TEvent>) => Element[] | Element | undefined | null;
